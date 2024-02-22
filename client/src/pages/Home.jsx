929fd2c591ed005873e77fba8a949dc5336761bf
James Parker <jamesparker.us@gmail.com>
2024-02-22 17:48:22 -0500

    added all carousel images on homepage

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,11 @@
+import React from "react";
+import Carousel from "../components/Carousel";
+
 const Home = () => {
   return (
     <div>
-      <h1>Welcome to Pittsburgh Doors & Closets</h1>
+      {/* <h1>Welcome to Pittsburgh Doors & Closets</h1> */}
+      <Carousel />
     </div>
   );
 };
