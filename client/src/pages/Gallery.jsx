2eae33c0b0ab7ee72dc28114965315b8b4c6f8da
James Parker <jamesparker.us@gmail.com>
2024-02-27 19:20:05 -0500

    Add Video to Gallery

--- client/src/pages/Gallery.jsx
+++ client/src/pages/Gallery.jsx
@@ -2,18 +2,17 @@ import React from "react";
 import ServiceVideoCard from "../components/ServiceVideoCard";
 import BulletCard from "../components/BulletCard";
 import Carousel_Gallery from "../components/Carousel_Gallery";
-// import videoFile from "../assets/PCD_Storage_Spaces_Vid.mp4";
 import { Link } from "react-router-dom";
 
 const Gallery = () => {
   return (
-    // The ServiceVideoCard
     <div>
-      {/* <ServiceVideoCard
-        video={videoFile}
+      {/* The ServiceVideoCard */}
+      <ServiceVideoCard
+        videoId="0Huazog37Qs"
         header="Discover the beauty of a perfectly organized home."
         description="The perfect combination of Quality & Beauty, tailored to your style and budget!"
-      /> */}
+      />
 
       {/* The BulletCard */}
       <BulletCard
