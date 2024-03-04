e9a23914c15819557082895863b00ff1366e2fdc
Moises Dobarganes <designerexpert@gmail.com>
2024-03-03 21:29:44 -0500

    footer responsive css

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,79 +1,60 @@
-import React from "react";
-import logo from "../assets/footer_logo_image.png";
+import React from 'react';
+import logo from '../assets/footer_logo_image.png';
 
 const Footer = () => {
   return (
-    <footer
-      className="footer"
-      style={{ borderTop: "1px solid #ccc", paddingTop: "50px" }}
-    >
-      <div className="footer-column">
-        <img src={logo} alt="Logo" style={{ width: "15%", height: "auto" }} />
-        <div className="footer-content">
-          <p style={{ marginBottom: ".1px", fontSize: "18px" }}>
-            Address: Pittsburgh Closets & Doors
-          </p>
-          <p style={{ marginBottom: "10px", fontSize: "18px" }}>
-            1916 Babcock Blvd., Pittsburgh, PA 15209
-          </p>
-          <p style={{ marginBottom: "10px", fontSize: "18px" }}>
-            Office: 724-302-9514
-          </p>
-          <p style={{ marginBottom: "10px", fontSize: "18px" }}>
-            Showroom: 724-209-4680
-          </p>
-          <p style={{ marginBottom: "10px", fontSize: "18px" }}>
-            License: #PA140165
-          </p>
+    <>
+      <footer className="footer" style={{ borderTop: '1px solid #ccc', paddingTop: '50px' }}>
+        <div className="footer-column">
+          <img src={logo} alt="Logo" style={{ width: '15%', height: 'auto' }} />
+          <div className="footer-content">
+            <p style={{ marginBottom: '.1px', fontSize: '18px' }}>Address: Pittsburgh Closets & Doors</p>
+            <p style={{ marginBottom: '10px', fontSize: '18px' }}>1916 Babcock Blvd., Pittsburgh, PA 15209</p>
+            <p style={{ marginBottom: '10px', fontSize: '18px' }}>Office: 724-302-9514</p>
+            <p style={{ marginBottom: '10px', fontSize: '18px' }}>Showroom: 724-209-4680</p>
+            <p style={{ marginBottom: '10px', fontSize: '18px' }}>License: #PA140165</p>
+          </div>
         </div>
-      </div>
 
-      <div className="footer-column">
-        <div className="footer-header">Social Media</div>
-        <div className="footer-content">
-          <p style={{ marginBottom: "10px", fontSize: "18px" }}>
-            <a
-              href="https://www.facebook.com/profile.php?id=61556068557103"
-              style={{ color: "#874c62" }}
-            >
-              Facebook
-            </a>
-          </p>
-          <p style={{ marginBottom: "260px", fontSize: "18px" }}>
-            <a
-              href="https://www.instagram.com/pittsburghclosetsanddoors/"
-              style={{ color: "#874c62" }}
-            >
-              Instagram
-            </a>
-          </p>
-          <p style={{ marginBottom: "20px", fontSize: "18px" }}>
-            Copyright 2024
-          </p>
+        <div className="footer-column">
+          <div className="footer-header">Social Media</div>
+          <div className="footer-content">
+            <p>
+              <a href="https://www.facebook.com/profile.php?id=61556068557103" style={{ color: '#874c62' }}>
+                Facebook
+              </a>
+            </p>
+            <p>
+              <a href="https://www.instagram.com/pittsburghclosetsanddoors/" style={{ color: '#874c62' }}>
+                Instagram
+              </a>
+            </p>
+          </div>
         </div>
-      </div>
 
-      <div className="footer-column">
-        <div className="footer-header">About Us</div>
-        <div className="footer-content">
-          <p style={{ marginBottom: "10px", fontSize: "18px" }}>
-            {/* <a href="/learn-about-us" style={{ color: "#874c62" }}>
+        <div className="footer-column">
+          <div className="footer-header">About Us</div>
+          <div className="footer-content">
+            <p style={{ marginBottom: '10px', fontSize: '18px' }}>
+              {/* <a href="/learn-about-us" style={{ color: "#874c62" }}>
               Learn About Us
             </a> */}
-          </p>
-          <p style={{ marginBottom: "10px", fontSize: "18px" }}>
-            <a href="/careers" style={{ color: "#874c62" }}>
-              Careers
-            </a>
-          </p>
-          <p style={{ marginBottom: "10px", fontSize: "18px" }}>
-            <a href="/privacy-policy" style={{ color: "#874c62" }}>
-              Privacy Policy
-            </a>
-          </p>
+            </p>
+            <p style={{ marginBottom: '10px', fontSize: '18px' }}>
+              <a href="/careers" style={{ color: '#874c62' }}>
+                Careers
+              </a>
+            </p>
+            <p style={{ marginBottom: '10px', fontSize: '18px' }}>
+              <a href="/privacy-policy" style={{ color: '#874c62' }}>
+                Privacy Policy
+              </a>
+            </p>
+          </div>
         </div>
-      </div>
-    </footer>
+      </footer>
+      <p>Copyright 2024</p>
+    </>
   );
 };
 
