0164735bc93c43d40f7315103e56d823eb81dec4
Moises Dobarganes <designerexpert@gmail.com>
2024-03-04 14:34:08 -0500

    added blog ui and routes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,15 @@
 // DO NOT TOUCH!
 
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import './scss/styles.scss';
+import './Blog.css';
 
-import App from "./App.jsx";
-import "./index.css";
+import App from './App.jsx';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
